e76349c4fa8e5b993c9c4bdf8b32bfbc66f3409f
clnt <c.connery14@gmail.com>
2024-11-10 18:38:53 +0100

    Authform, sign-in and sign-up ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl">StoreIt - The only storage solution you need</h1>
+      <h1 className="flex-center h-screen">
+        StoreIt - The only storage solution you need
+      </h1>
     </div>
   );
 }
