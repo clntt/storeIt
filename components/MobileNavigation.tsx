1854b19a7f1b333d74590c1d62806c2bb9df4904
clnt <c.connery14@gmail.com>
2024-11-15 13:28:30 +0100

    complete user sign in, signup and register

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -14,6 +14,7 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import FileUploader from "./FileUploader";
+import { signOutUser } from "@/lib/actions/user.action";
 
 interface Props {
   fullName: string;
@@ -107,7 +108,7 @@ const MobileNavigation = ({
             <Button
               type="submit"
               className="mobile-sign-out-button"
-              onClick={() => {}}
+              onClick={async () => await signOutUser()}
             >
               <Image
                 src="/assets/icons/logout.svg"
